19a17d7ccdbd30811e8bfa92fd522c02be4f81aa
SangkiHan <tkdrl8908@naver.com>
2026-06-05 11:03:51 +0900

    feat: Dockerfile

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["https://log-agent.sangkihan.co.kr", "http://localhost:6000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
